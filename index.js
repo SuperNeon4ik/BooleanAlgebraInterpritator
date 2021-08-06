c2076d5b20965c5a7f18767ba88b1a1441a62784
BuildTools <unconfigured@null.spigotmc.org>
2021-08-06 16:26:02 +0300

    + ChatColor is now working + readline.close() if file is provided

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ const readline = rl.createInterface({
 var debugMode = process.argv.findIndex(el => el.toLowerCase() == "--debug-mode") > -1;
 
 // Output info about the package
-console.log(`${packageConfig.name} by ${packageConfig.author}.\nVersion : ${packageConfig.version}`);
-if (debugMode) console.log("WARNING !!! Debug Mode is enabled.");
+console.log(`${packageConfig.name} by ${packageConfig.author}.\nVersion : ${packageConfig.version}\n`);
+if (debugMode) console.log(ChatColor.generateConsolePrefix("&3b"), "WARNING !!! Debug Mode is enabled.\n");
 
 // DEBUG : Output handled arguments
-if (debugMode) console.log("Handled arguments : " + process.argv);
+if (debugMode) ChatColor.log("&6bHandled arguments : &0a[ " + process.argv.join(", ") + " ]");
+
+// Check if file is provided in arguments
 if (process.argv.length < (debugMode ? 4 : 3)) {
     // Has file path provided
+    readline.close();
 }
 else {
     // Has no file path provided
--- modules/ChatColor.js
+++ modules/ChatColor.js
@@ -1,3 +1,7 @@
+const colorRegex = /(\\x1b\[[0-8,30-37,40-47]m)*/g;
+const codeRegex = /((?<!\\)\&[[0-6]a,[0-7][b,c]])*/g;
+
+// Main Colors
 const RESET = "\x1b[0m";
 const BRIGHT = "\x1b[1m";
 const DIM = "\x1b[2m";
@@ -6,6 +10,7 @@ const BLINK = "\x1b[5m";
 const REVERSE = "\x1b[7m";
 const HIDDEN = "\x1b[8m";
 
+// ForeGround (text) Colors
 const FG_BLACK = "\x1b[30m";
 const FG_RED = "\x1b[31m";
 const FG_GREEN = "\x1b[32m";
@@ -15,6 +20,7 @@ const FG_MAGENTA = "\x1b[35m";
 const FG_CYAN = "\x1b[36m";
 const FG_WHITE = "\x1b[37m";
 
+// BackGround Colors
 const BG_BLACK = "\x1b[40m";
 const BG_RED = "\x1b[41m";
 const BG_GREEN = "\x1b[42m";
@@ -23,3 +29,63 @@ const BG_BLUE = "\x1b[44m";
 const BG_MAGENTA = "\x1b[45m";
 const BG_CYAN = "\x1b[46m";
 const BG_WHITE = "\x1b[47m";
+
+// Color Codes
+const ColorCodes = {
+    "&0a": RESET,
+    "&1a": BRIGHT,
+    "&2a": DIM,
+    "&3a": UNDERSCORE,
+    "&4a": BLINK,
+    "&5a": REVERSE,
+    "&6a": HIDDEN,
+
+    "&0b": FG_BLACK,
+    "&1b": FG_RED,
+    "&2b": FG_GREEN,
+    "&3b": FG_YELLOW,
+    "&4b": FG_BLUE,
+    "&5b": FG_MAGENTA,
+    "&6b": FG_CYAN,
+    "&7b": FG_WHITE,
+
+    "&0c": BG_BLACK,
+    "&1c": BG_RED,
+    "&2c": BG_GREEN,
+    "&3c": BG_YELLOW,
+    "&4c": BG_BLUE,
+    "&5c": BG_MAGENTA,
+    "&6c": BG_CYAN,
+    "&7c": BG_WHITE,
+};
+
+function generateConsolePrefix(color) {
+    let match = codeRegex.test(color);
+    if (match) color = ColorCodes[color];
+
+    match = colorRegex.test(color);
+    if (match) return color + "%s" + RESET;
+    else return RESET;
+}
+
+function replaceColorCodes(input) {
+    let matches = input.match(/&\d[a,b,c]/g);
+
+    matches.forEach(el => {
+        if (ColorCodes[el] != undefined) input = input.replace(el, ColorCodes[el]);
+    });
+    
+    return input + RESET;
+}
+
+function log(input) {
+    console.log(replaceColorCodes(input) + RESET);
+}
+
+module.exports = { 
+    RESET, BRIGHT, DIM, UNDERSCORE, BLINK, REVERSE, HIDDEN,
+    FG_BLACK, FG_RED, FG_GREEN, FG_YELLOW, FG_BLUE, FG_MAGENTA, FG_CYAN, FG_WHITE,
+    BG_BLACK, BG_RED, BG_GREEN, BG_YELLOW, BG_BLUE, BG_MAGENTA, BG_CYAN, BG_WHITE,
+
+    generateConsolePrefix, replaceColorCodes, log
+};
