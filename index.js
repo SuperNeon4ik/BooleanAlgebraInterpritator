36d5de324096ce2af28fb906db69c4b1dc641d02
BuildTools <unconfigured@null.spigotmc.org>
2021-08-06 14:28:36 +0300

    Update index.js Added debugMode, ChatColor

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-var packageConfig = require("./package.json");
+// Initializing Libraries & Packages
+const packageConfig = require("./package.json");
+const rl = require("readline");
+const ChatColor = require("./modules/ChatColor");
 
+// Initializing readline
+const readline = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Initialize variables
+var debugMode = process.argv.findIndex(el => el.toLowerCase() == "--debug-mode") > -1;
+
+// Output info about the package
 console.log(`${packageConfig.name} by ${packageConfig.author}.\nVersion : ${packageConfig.version}`);
+if (debugMode) console.log("WARNING !!! Debug Mode is enabled.");
+
+// DEBUG : Output handled arguments
+if (debugMode) console.log("Handled arguments : " + process.argv);
+if (process.argv.length < (debugMode ? 4 : 3)) {
+    // Has file path provided
+}
+else {
+    // Has no file path provided
+}
